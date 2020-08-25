dc58c9788f279a80b3923302e03f2b75fbe19a26
Xenath <armemesser.94@gmail.com>
2020-08-25 08:22:34 +0200

    Added 2D perlin noise.

--- Tests/Perlin_Noise_Tester.py
+++ Tests/Perlin_Noise_Tester.py
@@ -3,6 +3,19 @@ import matplotlib.pyplot as plt
 
 import Library.Noise as Noise
 
+
+perlinObject = Noise.Perlin2D(maxOctaves = 8)
+
+N_ROWS = 256
+N_COLONS = 256
+z = np.zeros((N_ROWS, N_COLONS))
+for row in range(N_ROWS):
+    for colon in range(N_COLONS):
+        z[row, colon] = perlinObject.SampleOctaves(colon / N_COLONS, row / N_ROWS, persistance = 0.9, initialOctavesToSkip = 2)
+plt.imshow(z)
+plt.show()
+
+'''
 N_ROWS = 128
 N_COLONS = 128
 
@@ -30,7 +43,7 @@ z /= np.max(np.max(z))
 
 plt.imshow(z)
 plt.show()
-
+'''
 
 
 
